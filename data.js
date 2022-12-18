932985bd23ea2109d0ea18c73261409c745e3234
Waelpu <waelkaraa55@gmail.com>
2022-12-18 12:39:11 +0100

    checkpoint mongoose

---
+++ data.js
@@ -0,0 +1,37 @@
+exports.dataArray = [
+    { 
+        name: "Chito",
+        age: 22,
+        favoriteFoods: ["Fruits", "Ice cream"], 
+    },
+    { 
+        name: "Ayo",
+    age: 24,
+    favoriteFoods: ["Sandwish", "Rice"],
+},
+    { 
+        name: "Jessica",
+    age: 27,
+    favoriteFoods: ["Chicken", "Chips"],
+},
+    { 
+        name: "Nenna",
+        age: 28,
+        favoriteFoods: ["Bacon", "Sharwama"],
+    },
+    {
+        name: "Cece",
+        age: 27,
+        favoriteFoods: ["Snacks", "Tea"],
+     },
+    { 
+        name: "Micheal",
+        age: 25,
+        favoriteFoods: ["Pancake", "Salad"],
+    },
+    {
+        name: "Isaac",
+        age: 40,
+        favoriteFoods: ["Tuna Steak", "Hot dog"],
+    },
+  ];
